6c4164476afeb08dc6a5c3379c89fbea9525f807
Anthony <awrodriguez.bc@gmail.com>
2023-03-07 18:51:07 +0100

    carrousel de skins hecho

--- src/pages/SpecificChampion.jsx
+++ src/pages/SpecificChampion.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState,useContext } from 'react'
 import {ChampionContext} from '../context/ChampionContext'
 import NameTitle from '../components/champs/NameTitle'
+import { GrFormPreviousLink , GrFormNextLink} from "react-icons/gr";
+import { RxDotFilled } from "react-icons/rx";
+
 function SpecificChampion() {
   const {champions, items, ChampionName,setChampionName} = useContext(ChampionContext)  
   const [dataChamp, setdataChamp] = useState({})
@@ -24,6 +27,26 @@ function SpecificChampion() {
         console.log(Skins)
     })
   },[])
+
+  const [CurrentIndex, setCurrentIndex] = useState(0)
+
+  const prevSlide = () => 
+  {
+    const isFirstSlide = CurrentIndex ===0;
+    const newIndex = isFirstSlide ? Skins.length -1 : CurrentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+  const nextSlide = () => 
+  {
+    const isLastSlide = CurrentIndex === Skins.length -1;
+    const newIndex = isLastSlide ? 0 : CurrentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
+
+  const goToSlide = (slideIndex) =>
+  {
+    setCurrentIndex(slideIndex);
+  }
   return (
     
   <>
@@ -43,15 +66,32 @@ function SpecificChampion() {
     </div>
 {/* ///////////////////////////////////////////////////////////////////////////// */}
   
-    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
+    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
       <div style={
         {
-          backgroundImage : `url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${ChampionName}_5.jpg)`
-        }} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'></div>
+          backgroundImage : `url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${ChampionName}_${Skins[CurrentIndex]}.jpg)`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'contain'}
+          } className='w-full h-full rounded-2xl bg-center duration-500'></div>
     {/* Left Arrow */}
-
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-40 text-2xl rounded-full bg-black/20 text-white cursor-pointer'>
+          <GrFormPreviousLink size={40} onClick={prevSlide}/>
+      </div>
     {/* Right Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-40 text-2xl rounded-full bg-black/20 text-white cursor-pointer'>
+        <GrFormNextLink size={40} onClick={nextSlide} />
+      </div>
 
+      <div className='flex top-4 justify-center py-2 '>
+        {Skins?.map((skin,slideIndex)=>(
+          <div 
+          key={slideIndex} 
+          className='text-2xk cursor-pointer' 
+          onClick={() => goToSlide(slideIndex)}>
+            <RxDotFilled/>
+          </div>
+        ))}
+      </div>
     </div>
   {/* ///////////////////////////////////////////////////////////////////////////// */}
   </div>
